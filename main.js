9d79b814624f99bf3057661f9962a0dda76dac11
kmilivoj <milivoj.kalafatic@gmail.com>
2020-12-06 20:00:26 +0100

    port from enironment variable

--- main.js
+++ main.js
@@ -7,9 +7,14 @@ require('dotenv').config();
 // console.log(process.env);
 
 const app = express();
-app.listen(4000, () => console.log('listening at 4000'))
+// for deploy at cloud Glitch
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {console.log(`Starting server at ${port}`);
+});
+
 // npm install -g nodemon to no need to node index.js every time you change code
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(express.json({ limit: '1mb'}));
 
 
